f17a803f279392ee1a12312c1b28bd351e645127
bruhenri12 <bruhenri12@hotmail.com>
2023-04-16 19:18:52 -0300

    questao 1 finalizada - problemas

--- questao1/Aeroporto.java
+++ questao1/Aeroporto.java
@@ -4,20 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-class Aeroporto {
-    private static Lock lock = new ReentrantLock();
-    private static Condition condition = lock.newCondition();
+public class Aeroporto {
+
+    private static Scanner inputs = new Scanner(System.in);
+    
+    private static int totalPistas;
+    
+    public static List<Aviao> avioes = new ArrayList<Aviao>();
+    public static List<Pista> pistas = new ArrayList<Pista>();
+    
     public static void main(String[] args) {
 
-        Scanner inputs = new Scanner(System.in);
 
-        List<Aviao> avioes = new ArrayList<Aviao>();
-        List<Pista> pistas = new ArrayList<Pista>();
-        
         System.out.println("Quantidade de avioes saindo: ");
         int paraDecolar = inputs.nextInt();
         
@@ -26,10 +25,10 @@ class Aeroporto {
         System.out.println("Quantidade de avioes chegando: ");
         int paraChegar = inputs.nextInt();
         
-        avioes = appendAvioes(avioes,paraDecolar,"chegando");
+        avioes = appendAvioes(avioes,paraChegar,"chegando");
 
         System.out.println("Quantidade de pistas disponiveis: ");
-        int totalPistas = inputs.nextInt();
+        totalPistas = inputs.nextInt();
         
         inputs.close();
 
@@ -39,12 +38,12 @@ class Aeroporto {
             pistas.add(new Pista(i));
         }
 
-        controlePista(avioes, pistas);
+        for (int i = 0; i < totalPistas; i++) {
+            pistas.get(i).threadStart();
+        }
     }
 
     private static List<Aviao> appendAvioes(List<Aviao> avioes, int quantidade, String status) {
-        
-        Scanner inputs = new Scanner(System.in);
 
         for (int i = 0; i < quantidade; i++) {
             System.out.println("Horario do aviao " + (i+1) + ": ");
@@ -52,8 +51,6 @@ class Aeroporto {
 
             avioes.add(new Aviao(saidaAviao, status));
         }
-        
-        inputs.close();
 
         return avioes;
     }
@@ -61,16 +58,5 @@ class Aeroporto {
     private static List<Aviao> sortAviaoArr(List<Aviao> arrAviao) {
         Collections.sort(arrAviao);
         return arrAviao;
-    }
-
-    private static void controlePista(List<Aviao> avioes, List<Pista> pistas) throws InterruptedException{
-        lock.lock();
-        try {
-            while (!avioes.isEmpty()) {
-
-            }
-        } finally {
-            lock.unlock();
-        }
     }
 }
--- questao1/Pista.java
+++ questao1/Pista.java
@@ -1,9 +1,18 @@
 package questao1;
 
-public class Pista implements Runnable{
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Pista {
     private int pista;
-    private boolean ocupada = false;
     private Aviao aviao;
+    
+    private static boolean ocupada = true;
+    private static Lock lock = new ReentrantLock();
+    private static Condition pistaLivre = lock.newCondition();
+    private static Condition pistaOcupada = lock.newCondition();
 
     public Pista(int pistaId) {
         this.pista = pistaId;
@@ -14,16 +23,97 @@ public class Pista implements Runnable{
     }
 
     public int getPista() {
-        return this.pista;
+        return pista;
+    }
+
+    public Aviao getAviao() {
+        return aviao;
     }
 
     public boolean isOcupada() {
-        return this.ocupada;
+        return ocupada;
     }
 
-    @Override
-    public void run() {
+    public void threadStart() {
+        (new Thread(new LiberaPista())).start();
+        (new Thread(new OcupaPista())).start();
+    }
+
+    public static class LiberaPista implements Runnable {
+
+        public void operacao() throws InterruptedException {
+
+            lock.lock();
+            try {
+                while (!ocupada) {
+                    pistaOcupada.await();
+                }
+                
+                Aviao aviaoAtual = Aeroporto.avioes.get(0);
+
+                long horarioEsperado = aviaoAtual.getHorario();
+                String status = aviaoAtual.getStatus();
+
+                long horarioReal = System.currentTimeMillis();
+
+                System.out.println(
+                    "Aviao para " + horarioEsperado + ", " + status + " as " + 
+                    horarioReal + " com " + (horarioReal-horarioEsperado) + 
+                    " de atraso."
+                );
+
+                Aeroporto.avioes.remove(0);
+
+                ocupada = false;
+                pistaLivre.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                operacao();
+            } catch (InterruptedException e) {
+                System.err.println("Erro libera");
+            }
+        }
+    }
+
+    public static class OcupaPista implements Runnable {
         
-        System.out.println("Aviao ");
+        public void operacao() throws InterruptedException {
+
+            lock.lock();
+            try {
+                while (ocupada) {
+                    pistaLivre.await();
+                }
+                
+                Aviao aviaoAtual = Aeroporto.avioes.get(0);
+                long horarioEsperado = aviaoAtual.getHorario();
+                long horarioReal = System.currentTimeMillis();
+                
+                while (horarioReal <= horarioEsperado) {
+                }
+                
+                Thread.sleep(500);
+                
+                ocupada = true;
+                pistaOcupada.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                operacao();
+            } catch (InterruptedException e) {
+                System.err.println("Erro ocupa");
+            }
+        }
     }
 }
