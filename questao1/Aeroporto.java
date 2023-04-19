4d508d9aea8c4f9bba2fed4d440441e7bb73d4d8
bruhenri12 <bruhenri12@hotmail.com>
2023-04-19 03:18:56 -0300

    Questao 1 finalizada

--- questao1/Aeroporto.java
+++ questao1/Aeroporto.java
@@ -1,64 +1,147 @@
-package questao1;
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Aeroporto {
-
+    public static long horaInicial;
     public static int totalPistas;
-    public static long horarioRealStart;
-    public static int pistaLivreId;
 
-    private static Scanner inputs = new Scanner(System.in);
-    
-    public static List<Aviao> avioes = new ArrayList<Aviao>();
+    private static Scanner rpnRead;
+
+    public static List<Aviao> avioes = new ArrayList<>();
     public static List<Pista> pistas = new ArrayList<Pista>();
-    
+
+    private static final Lock lock = new ReentrantLock();
+    private static final Condition pistaLivre = lock.newCondition();
+    private static final Condition pistaOcupada = lock.newCondition();
+
     public static void main(String[] args) {
-
-
-        System.out.print("Quantidade de avioes saindo: ");
-        int paraDecolar = inputs.nextInt();
-        
-        avioes = appendAvioes(avioes,paraDecolar,"saindo");
-
-        System.out.print("Quantidade de avioes chegando: ");
-        int paraChegar = inputs.nextInt();
-        
-        avioes = appendAvioes(avioes,paraChegar,"chegando");
-
-        System.out.print("Quantidade de pistas disponiveis: ");
-        totalPistas = inputs.nextInt();
-        
-        inputs.close();
-
-        avioes = sortAviaoArr(avioes);
-
-        for (int i = 1; i < totalPistas; i++) {
-            pistas.add(new Pista(i));
+        try {
+            getInputs();
+        } catch (FileNotFoundException e) {
+            System.err.println("Arquivo nao encontrado.");
         }
-        
-        horarioRealStart = System.currentTimeMillis();
 
-        Controlador.Operador();
+        for (int i = 0; i < totalPistas; i++) {
+            pistas.add(new Pista());
+        }
+
+        Collections.sort(avioes);
+
+        horaInicial = System.currentTimeMillis();
+
+        for (int i = 0; i < avioes.size(); i++) {
+            Thread av = new Thread(avioes.get(i));
+            av.start();
+        }
+
+
     }
 
-    private static List<Aviao> appendAvioes(List<Aviao> avioes, int quantidade, String status) {
+    private static void appendAvioes(int quantidade, String status) {
 
         for (int i = 0; i < quantidade; i++) {
             System.out.print("Horario do aviao " + (i+1) + ": ");
-            long saidaAviao = inputs.nextLong();
+            long saidaAviao = Long.parseLong(rpnRead.nextLine());
+            System.out.println(saidaAviao);
 
             avioes.add(new Aviao(saidaAviao, status));
         }
-
-        return avioes;
     }
 
-    private static List<Aviao> sortAviaoArr(List<Aviao> arrAviao) {
-        Collections.sort(arrAviao);
-        return arrAviao;
+    private static void getInputs() throws FileNotFoundException {
+
+        System.out.println("Insira o caminho do arquivo teste:");
+        Scanner usrInput = new Scanner(System.in);
+        String fileName = usrInput.nextLine();
+        usrInput.close();
+
+        File rpnFile = new File(fileName);
+        rpnRead = new Scanner(rpnFile);
+
+        System.out.println("Entrada:");
+
+        System.out.print("Quantidade de avioes saindo: ");
+        int paraDecolar = Integer.parseInt(rpnRead.nextLine());
+        System.out.println(paraDecolar);
+
+        appendAvioes(paraDecolar,"saindo");
+
+        System.out.print("Quantidade de avioes chegando: ");
+        int paraChegar = Integer.parseInt(rpnRead.nextLine());
+        System.out.println(paraChegar);
+
+        appendAvioes(paraChegar,"chegando");
+
+        System.out.print("Quantidade de pistas disponiveis: ");
+        totalPistas = Integer.parseInt(rpnRead.nextLine());
+        System.out.println(totalPistas);
+
+        rpnRead.close();
+    }
+
+    public static class Pista {
+
+    }
+    public static class Aviao implements Comparable<Aviao>, Runnable {
+        private final long horario;
+        private final String status;
+
+
+        public Aviao(long hr, String st) {
+            this.horario = hr;
+            this.status = st;
+        }
+
+        @Override
+        public int compareTo(Aviao av) {
+            return (int) this.horario - (int) av.horario;
+        }
+
+        @Override
+        public void run() {
+            try {
+                print();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void print() throws InterruptedException {
+            lock.lock();
+            try {
+                long horaAtual = System.currentTimeMillis() - horaInicial;
+
+                if (horaAtual < this.horario) {
+                    Thread.sleep(this.horario-horaAtual);
+                }
+                if (pistas.isEmpty()) {
+                    pistaLivre.await();
+                }
+                pistas.remove(0);
+
+                Thread.sleep(500);
+                horaAtual = System.currentTimeMillis() - horaInicial;
+
+                long atraso = horaAtual - (this.horario + 500);
+
+                System.out.print("Aviao para " + this.horario);
+                System.out.print(" " + this.status);
+                System.out.print(" as " + horaAtual);
+                System.out.println(" com atraso de " + atraso);
+
+                pistas.add(new Pista());
+
+                pistaLivre.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 }
